32849933030516a30bf6336d9354f3db7566feaf
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-20 14:22:08 -0400

    first commit

---
+++ db/seed.js
@@ -0,0 +1,18 @@
+// Import the database connection
+const mongoose = require('./connection')
+
+///////////////////////////////////////////
+// IMPORT YOUR MODELS BELOW
+///////////////////////////////////////////
+
+///////////////////////////////////////////
+// DO YOUR DATABASE OPERATIONS IN BELOW FUNCTION
+///////////////////////////////////////////
+
+const seed = async () => {
+   //*********Code Goes Here
+  //***************************** */   
+}
+
+// Run Seed Function
+seed()
