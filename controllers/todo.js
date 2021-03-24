20cb136207949575fb2eb12df0e03dee107c5ceb
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-23 21:02:48 -0400

    update index.js with delete

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,54 +1,66 @@
-////////////////////////
-// Import Models
-////////////////////////
-const List = require("../models/List")
-const Todo = require("../models/Todo")
-///////////////////////////
-// Controller Functions
-///////////////////////////
+/////////////////////////
+// import models
+/////////////////////////
+const Todo = require("../models/Todo");
+const List = require("../models/List");
 
-// index controller = get all todos
+/////////////////////////
+// controller functions
+/////////////////////////
+
+// Main Todo Page
 const index = async (req, res) => {
+  const lists = await List.find({}).populate("todos");
 
-    const lists = await List.find({}).populate('todos')
-    res.render("todo/index", {lists})
+  res.render("todo/index", { lists });
+};
+
+// New Todo Page
+const newTodo = async (req, res) => {
+  // Retrieve List, so user can select which list to add todo to
+  const lists = await List.find({})
+  // render todo/new.ejs with the lists
+  res.render("todo/new", {lists});
+};
+
+// New Todo Page
+const create = async (req, res) => {
+  // try block to catch any errors
+  try {
+    // get the target list
+    const list = await List.findById(req.body.list);
+    // replace list string with list id object
+    req.body.list = list;
+    // create the new todo using the request body
+    const todo = await Todo.create(req.body);
+    // add the todo to lists todos array
+    list.todos.push(todo)
+    // save changes
+    list.save()
+    //redirect back to main todos page
+    res.redirect("/todos");
+  } catch (error) {
+    // send error as json if there is one
+    res.json(error);
+  }
+};
+
+const destroy = async (req, res) => {
+    // save the param in a variable
+    const id = req.params.id
+    // delete the todo
+    await Todo.findByIdAndDelete(id)
+    // redirect back to main page
+    res.redirect("/todos")
 }
 
-// new Todo Page
-const newTodo = async (req, res) => {
-    // Retrieve List, so user can select which list to add todo to
-    const lists = await List.find({})
-    // render todo/new.ejs with the lists
-    res.render('todo/new', {lists});
-};
+/////////////////////////
+// export controller functions
+/////////////////////////
 
-// new To Do Page
-const create = async (req, res) => {
-    // try block to catch any errors
-    try {
-        // get the target list
-        const list = await List.findById(req.body.list);
-        // replace list string with list id object
-        req.body.list = list;
-        // create the new To Do using the request body
-        const todo = await Todo.create(req.body);
-        // add the todo to lists todos array
-        list.todos.push(todo)
-        // save changes
-        list.save()
-        // recirect back to main todos page
-        res.redirect('/todos';)
-    } catch (error) {
-        // send error as json if there is one
-        res.json(error);
-    }
-};
-
-//////////////////////////////
-// Export Controller
-//////////////////////////////
 module.exports = {
-    index,
-    new: newTodo,
-    create
+  index,
+  new: newTodo,
+  create,
+  destroy
 };
--- routes/todo.js
+++ routes/todo.js
@@ -16,6 +16,12 @@ router.get("/", TodoController.index)
 // new todo page
 router.get('/new', TodoController.new)
 
+// post request to create a new To Do
+router.post('/', TodoController.create)
+
+// delete request to delete a new todo
+router.delete("/:id", TodoController.destroy)
+
 //////////////////////////////////
 // Export the router
 //////////////////////////////////
