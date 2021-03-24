f689f9ff2688c077573d23cfcac45cd8e456d129
Jamie Roszel <76765652+jamieroszel@users.noreply.github.com>
2021-03-24 10:31:24 -0400

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 const { log } = require("mercedlogger")
 // Bring in our database string from .env or default string
 const MONGODB_URL =
-  process.env.MONGODB_URL || "mongodb+srv://new_user:Ykudnr6UXYQEDGT@cluster0.tecvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  process.env.MONGODB_URL
 
 ///////////////////////////////////
 // Mongoose Configuration Object to Avoid Warnings
